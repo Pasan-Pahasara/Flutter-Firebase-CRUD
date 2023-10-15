d85f2b83fb1aa7579031d5b565817fd62b824c18
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-10-16 01:20:02 +0530

    implement get collection of notes

--- lib/services/firestore/firestore.dart
+++ lib/services/firestore/firestore.dart
@@ -1,3 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class FirestoreService {
-  
+  // Get collection of notes
+  final CollectionReference notesCollection =
+      FirebaseFirestore.instance.collection('notes');
 }
