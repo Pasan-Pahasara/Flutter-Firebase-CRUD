ea7e70a9b6b74ab49e375068f82215d51288c85d
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-10-16 01:23:03 +0530

    implement CREATE Function

--- lib/services/firestore/firestore.dart
+++ lib/services/firestore/firestore.dart
@@ -4,4 +4,12 @@ class FirestoreService {
   // Get collection of notes
   final CollectionReference notesCollection =
       FirebaseFirestore.instance.collection('notes');
+
+  // CREATE: Add a new note
+  Future<void> addNote(String note) {
+    return notesCollection.add({
+      'note': note,
+      'timestamp': Timestamp.now(),
+    });
+  }
 }
