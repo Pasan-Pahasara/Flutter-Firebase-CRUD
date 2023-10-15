bc4e79573a71727a2b6d920eda87f427eff32200
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-10-15 23:54:49 +0530

    introducing home page

--- lib/ui/root_page/root_page.dart
+++ lib/ui/root_page/root_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../home_page/home_page.dart';
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -7,6 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
